d710c4f821d5b893df9afff771a3f990f0724356
ehharvey <36806437+ehharvey@users.noreply.github.com>
2022-09-22 14:29:28 -0700

    Reads student data
    * Reads StudentData.txt
    * Stores first and last names from txt file
    * Stores all student data in a vector

--- CSCN73030-Assignment2-eharvey3704/Source.cpp
+++ CSCN73030-Assignment2-eharvey3704/Source.cpp
@@ -1,5 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+const std::string STUDENT_FILE = "StudentData.txt";
+
+struct StudentData
+{
+	std::string first_name;
+	std::string last_name;
+
+	StudentData(std::string raw_student_data, char delimiter = ',')
+	{
+		// Constructs a StudentData instance from a delimited string.
+		// delimiter defaults to ','
+
+
+		auto found_delimiter = raw_student_data.find(delimiter);
+
+		if (found_delimiter == std::string::npos)
+		{
+			throw std::invalid_argument("Delimiter not found in raw_student_data");
+		}
+		else
+		{
+			this->last_name = raw_student_data.substr(0, found_delimiter);
+			this->first_name = raw_student_data.substr(found_delimiter + 1);
+		}
+	}
+};
+
 
 int main()
 {
+	auto file = std::ifstream(STUDENT_FILE);
+
+	auto students = std::vector<StudentData>();
+
+	for (std::string line; std::getline(file, line);)
+	{
+		students.push_back(StudentData(line));
+	}
+	
 	return 1;
 }
